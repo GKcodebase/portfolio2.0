81217836866ddb8c2a79a793488b12a3645323dd
Gokul G.K <gokulgk1200@gmail.com>
2025-01-12 16:10:12 -0600

    fixing card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -20,55 +20,57 @@ export const Card: React.FC<ProjectCardProps> = ({
 }) => {
 
     return (
-        <Flex
-            fillWidth gap="s"
-            direction="column"
-            className={styles.card}
-        >
+        <SmartLink href={href}>
             <Flex
-                fillWidth paddingX="l" paddingTop="xs" paddingBottom="m" gap="l">
-                <Grid
-                    border="neutral-medium"
-                    columns="repeat(2, 1fr)"
-                    tabletColumns="1col"
-                    mobileColumns="1col"
-                    gap="xs"
-                    padding="xs"
-                    radius="l"
-                >
-                    {title && (
-                        <Flex
-                            flex={5}
-                            className={styles.cardleft}
-                        >
-                            <Heading
-                                as="h3"
-                                wrap="balance"
-                                variant="display-strong-xs">
-                                {title}
-                            </Heading>
-                            {github ?
-                                <Flex padding="xs">
-                                    <IconButton size="m" tooltip="click to see the project" icon="github" href={href} />
-                                </Flex>
-                                :
-                                <Flex padding="xs">
-                                    <IconButton size="m" tooltip="click to see the project" icon="book" href={href} />
-                                </Flex>
-                            }
-                            <Flex padding="xs">{content}</Flex>
-                        </Flex>
-                    )}
-                    {content && (
-                        <Flex
-                            flex={5}>
-                            <h4>
-                                {description}
-                            </h4>
-                        </Flex>
-                    )}
-                </Grid>
+                fillWidth gap="s"
+                direction="column"
+                className={styles.card}
+            >
+                <Flex
+                    fillWidth paddingX="l" paddingTop="xs" paddingBottom="m" gap="l">
+                    <Grid
+                        border="neutral-medium"
+                        tabletColumns="1col"
+                        mobileColumns="1col"
+                        gap="xs"
+                        padding="xs"
+                        radius="l"
+                    >
+                        {title && (
+                            <Flex
+                                flex={5}
+                                className={styles.cardleft}
+                            >
+                                <Heading
+                                    as="h2"
+                                    wrap="balance"
+                                    variant="heading-strong-xs">
+                                    {title}
+                                </Heading>
+                                {github ?
+                                    <Flex padding="xs">
+                                        <IconButton size="m" tooltip="click to see the project" icon="github" href={href} />
+                                        <Flex padding="xs">{content}</Flex>
+                                    </Flex>
+                                    :
+                                    <Flex padding="xs">
+                                        <IconButton size="m" tooltip="click to see the project" icon="book" href={href} />
+                                        <Flex padding="xs">{content}</Flex>
+                                    </Flex>
+                                }
+                            </Flex>
+                        )}
+                        {content && (
+                            <Flex
+                                flex={4}>
+                                <h4>
+                                    {description}
+                                </h4>
+                            </Flex>
+                        )}
+                    </Grid>
+                </Flex>
             </Flex>
-        </Flex>
+        </SmartLink>
     );
 };
